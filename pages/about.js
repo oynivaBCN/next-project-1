f1bf915f64bed78715c81075f527e257a1440964
Pandora <pandora.knocker@psious.com>
2021-02-22 16:22:30 +0100

    Try next Image

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,46 @@
+import Image from 'next/image'
+
 const about = () => {
     console.log('log from about page - this shows in browser console')
     return (
         <div>
             <p>A mini project created with Next.js that simulates a Web Dev new blog.</p>
+            <p>take up viewport space.</p>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>.</h1>
+            <h1>{`Image rendered with next <Image:/> `}</h1>
+            <Image src="/screenshot.png" width={700} height={300}/>
         </div>
     )
 }
